01a99d4bc4454dbcb5c795aae6b2b2f9706b99a0
Nikhil-Pan-dey <101667879+Nikhil-Pan-dey@users.noreply.github.com>
2022-09-24 15:23:57 +0530

    updated myconnection class

--- Project1/src/main/java/MyConnection.java
+++ Project1/src/main/java/MyConnection.java
@@ -6,7 +6,7 @@ public class MyConnection {
 	static public Connection getconnecton() throws ClassNotFoundException, SQLException {
 		Connection con;
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/npacademy","root","root");
+		con=DriverManager.getConnection("jdbc:mysql://ugklfhsz2j6devws:yNcvzGd0bCtVC8q2Veyo@bnjnbf0zxao8xxx0blqe-mysql.services.clever-cloud.com:3306/bnjnbf0zxao8xxx0blqe","ugklfhsz2j6devws","yNcvzGd0bCtVC8q2Veyo");
 		return con;
 		
 	}
