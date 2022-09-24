06961f694c04c35554b5de6d5557e1c4d2f61147
Nikhil-Pan-dey <101667879+Nikhil-Pan-dey@users.noreply.github.com>
2022-09-24 19:11:46 +0530

    correction of myConnection Class

--- Project1/src/main/java/MyConnection.java
+++ Project1/src/main/java/MyConnection.java
@@ -1,12 +1,16 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class MyConnection {
-	static public Connection getconnecton() throws ClassNotFoundException, SQLException {
-		Connection con;
+	static public Connection getconnecton(){
+		
+		Connection con = null;
+		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://ugklfhsz2j6devws:yNcvzGd0bCtVC8q2Veyo@bnjnbf0zxao8xxx0blqe-mysql.services.clever-cloud.com:3306/bnjnbf0zxao8xxx0blqe","ugklfhsz2j6devws","yNcvzGd0bCtVC8q2Veyo");
+			con=DriverManager.getConnection("jdbc:mysql://ugklfhsz2j6devws:yNcvzGd0bCtVC8q2Veyo@bnjnbf0zxao8xxx0blqe-mysql.services.clever-cloud.com:3306/bnjnbf0zxao8xxx0blqe","ugklfhsz2j6devws","yNcvzGd0bCtVC8q2Veyo");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return con;
 		
 	}
