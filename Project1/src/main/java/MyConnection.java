1fbca21fb620d1bb03d1f49020f256a0c3e32027
Nikhil-Pan-dey <101667879+Nikhil-Pan-dey@users.noreply.github.com>
2022-12-28 00:11:18 +0530

    Update MyConnection.java

---
+++ Project1/src/main/java/MyConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+	static public Connection getconnecton(){
+		
+		Connection con = null;
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://ugklfhsz2j6devws:yNcvzGd0bCtVC8q2Veyo@bnjnbf0zxao8xxx0blqe-mysql.services.clever-cloud.com:3306/bnjnbf0zxao8xxx0blqe","ugklfhsz2j6devws","yNcvzGd0bCtVC8q2Veyo");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+		
+	}
+}
