4c4fb584547c4e944a4358d6c08488c2f7188a68
Nikhil-Pan-dey <101667879+Nikhil-Pan-dey@users.noreply.github.com>
2022-12-28 00:13:44 +0530

    updating to html again

--- Project1/src/main/java/MyConnection.java
+++ Project1/src/main/java/MyConnection.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -15,3 +16,22 @@ public class MyConnection {
 		
 	}
 }
+=======
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+	static public Connection getconnecton(){
+		
+		Connection con = null;
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://ugklfhsz2j6devws:yNcvzGd0bCtVC8q2Veyo@bnjnbf0zxao8xxx0blqe-mysql.services.clever-cloud.com:3306/bnjnbf0zxao8xxx0blqe","ugklfhsz2j6devws","yNcvzGd0bCtVC8q2Veyo");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+		
+	}
+}
+>>>>>>> parent of fc9b14f (updating mysql connection)
